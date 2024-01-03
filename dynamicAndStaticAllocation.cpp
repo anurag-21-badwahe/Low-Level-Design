55cd6534054ecdcdf6ca1e846c68387ed6921888
anurag <anuragbadwahe@gmail.com>
2024-01-03 23:48:45 +0530

    Learn about static and Dynamic Allocation

---
+++ dynamicAndStaticAllocation.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+//Created a class
+class Hero{
+    public:
+ //Properties
+ int power;
+ char name[100];
+ char level;
+
+};
+
+
+int main(){
+
+   // Static Allocation
+    Hero obj;
+    cout << "Level of obj is " << obj.level << endl;
+
+   //Dynamically Allocation
+   Hero *obj2 = new Hero;
+   cout << "Level of obj is " << (*obj2).level << endl;
+   cout << "Level of obj is " << obj2->level << endl;
+
+
+}
