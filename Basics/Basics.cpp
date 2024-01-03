5b6c239c239f8400a1e18e0c70a70e79040325fe
anurag <anuragbadwahe@gmail.com>
2024-01-03 22:11:58 +0530

    Learn creation of class and object

---
+++ Basics/Basics.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//Created a class
+class Hero{
+ //Properties
+ int health;
+ char name[100];
+ char level;
+
+};
+
+
+int main(){
+
+   // Created object of Hero Type
+    Hero obj;
+
+    cout << sizeof(obj); //108
+    // Print the size that is taken by its Properties
+}
