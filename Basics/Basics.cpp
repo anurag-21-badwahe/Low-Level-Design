edcf2d3c56f478d4d8803ac74bebd8133b8c175b
anurag <anuragbadwahe@gmail.com>
2024-01-03 22:17:54 +0530

    Learn about Access Modifier

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 //Created a class
 class Hero{
+    public:
  //Properties
- int health;
+ int power;
  char name[100];
  char level;
 
@@ -16,6 +17,12 @@ int main(){
    // Created object of Hero Type
     Hero obj;
 
-    cout << sizeof(obj); //108
+    cout << sizeof(obj) << endl; //108
     // Print the size that is taken by its Properties
+
+
+    // Let's see how the access the class properties or data member
+
+    cout << "Name of obj is " << obj.name << endl;
+    cout << "Power of obj is " << obj.power << endl;
 }
